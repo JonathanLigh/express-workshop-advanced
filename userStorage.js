cb3fb314b555e64c560a1b18dd88fa59654f2140
Jason Wang <jason@wangjason.com>
2018-01-09 07:54:59 -0500

    more fixes

--- userStorage.js
+++ userStorage.js
@@ -1,21 +1,26 @@
 var data = {
   jason: {
+    name: 'jason',
     age: 43,
     salary: 3.50
   },
   bill: {
+    name: 'bill',
     age: 22,
     salary: 100
   },
   jon: {
+    name: 'jon',
     age: 25,
     salary: 500
   },
   aaron: {
+    name: 'aaron',
     age: 20,
     salary: 10
   },
   ethan: {
+    name: 'ethan',
     age: 22,
     salary: 0
   }
