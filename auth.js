677041169c1a631814832fb3c5f228de3855c3d3
Jason Wang <jason@wangjason.com>
2018-01-09 08:20:32 -0500

    final auth

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ module.exports = {
   // Notice this is just a route handler function we are exporting, normally called a controller
   checkAuth: function(req, res, next) {
     if (req.body.pass === secret) {
-      return next();
+      next();
     } else {
       //  I decided to respond with sending not authorized b/c it was more explicit.
       res.status(401).send('Not Authorized');
