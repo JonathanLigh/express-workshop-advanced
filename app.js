bade64976ed6cd3678f9655aaafbe8103739514a
Jason Wang <jason@wangjason.com>
2018-01-09 07:41:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var chalk = require('chalk');
 var bodyParser = require('body-parser');
 var volleyBall = require('volleyball');
 
-app.use(bodyParser.json());
-
 app.use(volleyBall);
+
+app.use(bodyParser.json());
 
 app.use('/api', require('./routes'));
 
